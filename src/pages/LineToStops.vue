20a1a30a6e651eb1f30a4565922869d4fe61b6fb
CakeAL <1742507190@qq.com>
2024-06-02 18:55:06 +0800

    ✨ feat(StopToLines): finished and added a click to jump function

--- src/pages/LineToStops.vue
+++ src/pages/LineToStops.vue
@@ -2,6 +2,7 @@
 import { BusOutline } from "@vicons/ionicons5";
 import { onMounted, ref } from "vue";
 import { invoke } from "@tauri-apps/api";
+import { store } from "../store.js";
 
 interface LineName {
   label: string;
@@ -17,6 +18,11 @@ const card_title = ref("");
 onMounted(() => {
   // 挂载时获取一次数据
   get_lines();
+  if (store.line_name !== "") {
+    select_line.value = store.line_name;
+    get_line_stops();
+    store.line_name = "";
+  }
 });
 
 const get_lines = async () => {
@@ -50,15 +56,22 @@ const get_line_stops = async () => {
   line_stops.value = JSON.parse(result);
   // console.log(line_stops.value);
   card_title.value =
+    select_line.value +
+    " " +
     line_stops.value[0][1] +
-    " —— " +
+    " -> " +
     line_stops.value[line_stops.value.length - 1][1];
+};
+
+const store_stop_name = (name) => {
+  store.stop_name = name;
+  window.location.href = '#/stop_to_lines';
 };
 </script>
 
 <template>
   <div class="container">
-    <p>试试在下面的输入框中输入并选择你想要查询的公交线路吧！</p>
+    <p>试试在下面的输入框中输入并选择你想要查询的<b>公交线路</b>吧！</p>
     <n-select
       filterable
       v-model:value="select_line"
@@ -78,9 +91,11 @@ const get_line_stops = async () => {
         <n-timeline-item
           v-for="(stop, index) in line_stops"
           :key="index"
+          @click="store_stop_name(stop[1])"
           color="#dc2533"
           :title="stop[0]"
           :content="stop[1]"
+          class="timeline-item"
         />
       </n-timeline>
     </n-card>
@@ -96,5 +111,9 @@ const get_line_stops = async () => {
   height: 60vh;
   overflow: auto;
   margin-top: 15px;
+}
+
+.timeline-item {
+  cursor: pointer;
 }
 </style>
--- src/pages/StopToLines.vue
+++ src/pages/StopToLines.vue
@@ -1,10 +1,94 @@
-<script setup></script>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { invoke } from "@tauri-apps/api";
+import { store } from "../store.js";
+
+interface StopName {
+  label: string;
+  value: string;
+}
+
+const select_stop = ref("");
+const stops_name = ref<Array<string>>([]);
+const options = ref<Array<StopName>>([]);
+const stop_lines = ref([]);
+
+onMounted(() => {
+  // 挂载时获取一次数据
+  get_stops();
+  if (store.stop_name !== "") {
+    select_stop.value = store.stop_name;
+    get_stop_lines();
+    store.stop_name = "";
+  }
+});
+
+const get_stops = async () => {
+  // 同 LineToStops.vue
+  const result = await invoke("search_stops_name", { keyword: "" }).catch(
+    (err) => console.log(err)
+  );
+  stops_name.value = JSON.parse(result as string);
+  stops_name.value.forEach((stop_name) => {
+    options.value.push({
+      label: stop_name,
+      value: stop_name,
+    });
+  });
+};
+
+const get_stop_lines = async () => {
+  const result = await invoke("search_the_stops_lines", {
+    stopName: select_stop.value,
+  }).catch((err) => console.log(err));
+  stop_lines.value = JSON.parse(result as string);
+};
+
+const store_line_name = (name) => {
+  store.line_name = name;
+};
+</script>
 
 <template>
-    <div>
-        <h1>StopToLines</h1>
-    </div>
+  <div class="container">
+    <p>试试在下面的输入框中输入并选择你想要查询的<b>公交站点</b>吧！</p>
+    <n-select
+      filterable
+      v-model:value="select_stop"
+      placeholder="站名"
+      :options="options"
+      @update:value="get_stop_lines"
+    >
+    </n-select>
+    <n-card class="show-lines" hoverable>
+      <n-space>
+        <a
+          v-for="(line, index) in stop_lines"
+          :key="index"
+          href="'#/line_to_stops"
+          @click="store_line_name(line)"
+        >
+          <n-tag :bordered="false" type="success" class="my-tag">
+            {{ line }}
+          </n-tag>
+        </a>
+      </n-space>
+    </n-card>
+  </div>
 </template>
 
 <style scoped>
+.container {
+  padding: 15px;
+}
+
+.show-lines {
+  height: 60vh;
+  overflow: auto;
+  margin-top: 15px;
+}
+
+.my-tag {
+  cursor: pointer;
+}
 </style>
